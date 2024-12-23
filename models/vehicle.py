c314d4f24916a847be5c4df6428cc54d89a99fe1
Blas Correa Rojas <bmx.correa@gmail.com>
2024-12-23 18:06:06 -0500

    [ADD] fleet_management: Task 2 - needs service compute

--- models/vehicle.py
+++ models/vehicle.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 FUEL_TYPE = [("gasoline", "Gasoline"), ("diesel", "Diesel"), ("electric", "Electric")]
 MONTHS_TO_SERVICE = 6
@@ -24,3 +24,20 @@ class Vehicle(models.Model):
         string="Last Service Date",
         default=fields.Date.today,
     )
+    needs_service = fields.Boolean(
+        string="Needs Service",
+        compute="_compute_needs_service",
+        store=True,
+    )
+
+    # COMPUTED FIELDS
+    @api.depends("mileage", "last_service_date")
+    def _compute_needs_service(self):
+        """Computes if the vehicle needs service according to the mileage
+        or the last service date."""
+        for vehicle in self:
+            vehicle.needs_service = (
+                vehicle.mileage > 20000
+                or (fields.Date.today() - vehicle.last_service_date).days
+                > MONTHS_TO_SERVICE * 30
+            )
